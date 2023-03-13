120ab81db15de076ad38ae408180d47b1805be90
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-03-13 12:43:46 +0100

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -13,7 +13,7 @@ var drops = [];
 for (var i = 0; i < columns; i++) {
     drops.push(0);
 }
-var str = "10001 010011 11010 1101 0011 0100 101 101 010 10 1011110 0111";
+var str = "1子b00不事0a1 c01通001d1 11功易0e10 1101 0匠f皆0g11 01功h易0i0 1j01 1k0子不l通m1 01x0 10輪z輿 101p事11o10 0uw梓n得1q11";
 function draw() {
     context.fillStyle = "rgba(10, 25, 47, 0.1)";
     context.fillRect(0, 0, W, H);
