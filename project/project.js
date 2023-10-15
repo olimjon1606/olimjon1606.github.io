84e1e01eae09d24f687e9ee7f86e9710ae24969f
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-10-15 18:16:33 +0200

    done

--- project/project.js
+++ project/project.js
@@ -11,22 +11,22 @@ const projects = [
     },
     {
         id: 2,
-        label: " pancakes",
-        title: ["breakfast", "https://olimjonorifjonov.com/",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye"],
-        img: "./images/image-code-box.png",
-        links: ["git", "insta"],
+        label: "Featured Project",
+        title: ["CodeBox_", "https://code-box-01.netlify.app/",],
+        details: "The Code Box Study Center Website is a dynamic and user-friendly web application designed to provide students and educators with a comprehensive platform for learning, collaboration, and resource sharing",
+        list: ["JavaScript", "SCSS", "CSS", "HTML"],
+        img: "./project/images/image-code-box.png",
+        links: ["https://github.com/olimjon1606/code-box", "https://code-box-01.netlify.app/"],
 
     },
     {
         id: 3,
-        label: " pancakes",
-        title: ["breakfast", "https://olimjonorifjonov.com/",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye"],
-        img: "./images/image-coffee.png",
-        links: ["git", "insta"],
+        label: "Featured Project",
+        title: ["Movies", "https://the-list-of-best-movies.netlify.app/",],
+        details: "Whether you're a seasoned film buff, a casual moviegoer, or just looking for an excellent movie night pick, our web app is here to guide you. Immerse yourself in a universe of movie information, from ratings and reviews to production dates and much more.",
+        list: ["JavaScript", "PHP", "SCSS", "CSS", "HTML"],
+        img: "./project/images/movie.png",
+        links: ["https://github.com/olimjon1606/list-of-best-movies", "https://the-list-of-best-movies.netlify.app/"],
 
     }
 
@@ -35,76 +35,75 @@ const projects = [
 const archive = [
     {
         id: 1,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
-
+        links: ["https://github.com/olimjon1606/online-coffee-shopp", "https://online-coffee-shop.netlify.app/"],
+        title: ["https://online-coffee-shop.netlify.app/", "Coffee Shop",],
+        details: "Designed for coffee connoisseurs, casual sippers, and café owners alike, this web application offers an immersive coffee experience that brings the aroma and ambiance of your favorite coffee shop right to your screen.",
+        list: ["JavaScript", "CSS", "HTML"]
     },
     {
         id: 2,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+        links: ["https://github.com/olimjon1606/dice_game", "https://playdice-game.netlify.app/"],
+        title: ["https://playdice-game.netlify.app/", "PlayDice",],
+        details: "The Dice Game is a browser-based application that allows users to play a virtual dice game. It's designed to be simple and intuitive, providing an enjoyable gaming experience for players of all ages.",
+        list: ["JavaScript", "WordPress", "CSS", "HTML"],
 
     },
     {
         id: 3,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+        links: ["https://github.com/olimjon1606/drum_kit", "https://my-drum-play.netlify.app"],
+        title: ["https://my-drum-play.netlify.app", "PlayDrum",],
+        details: "The Drum Game Web Application is a browser-based application designed to bring the joy of drumming to users of all skill levels.",
+        list: ["JavaScript", "WordPress", "CSS", "HTML"],
 
     },
     {
         id: 4,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+        links: ["https://github.com/olimjon1606/weather", "https://weather-forecasting-website.netlify.app"],
+        title: ["https://weather-forecasting-website.netlify.app", "Weather",],
+        details: "It provides a wide range of weather data and features to help users stay informed and make decisions related to travel, outdoor activities, and daily planning",
+        list: ["JavaScript", "OpenWeather_API"],
 
     },
     {
         id: 5,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+        links: ["https://github.com/olimjon1606/laruki", "https://laruki-my-website.netlify.app"],
+        title: ["https://laruki-my-website.netlify.app", "Laruki",],
+        details: "Laruki is an online platform that offers a comprehensive suite of tools and features for software developers, teams, and organizations.",
+        list: ["JavaScript", "CSS", "HTML"],
 
     },
     {
         id: 6,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+        links: ["https://github.com/olimjon1606/bella_food-store", "https://bellaa-my-website.netlify.app"],
+        title: ["https://bellaa-my-website.netlify.app", "Bella",],
+        details: "Bella Web Food Store is an online destination that offers a delightful and convenient food shopping experience.",
+        list: ["JavaScript", "SCSS", "CSS", "HTML"],
 
-    },
-    {
-        id: 7,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+    }
+    // {
+    //     id: 7,
+    //     links: ["https://olimjonorifjonov.com/", "https://acme-website-mic.netlify.app"],
+    //     title: ["https://acme-website-mic.netlify.app", "breakfast",],
+    //     details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    //     list: ["home", "hello", "bye", "asdsa", "dasds"],
 
-    },
-    {
-        id: 8,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+    // },
+    // {
+    //     id: 8,
+    //     links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
+    //     title: ["https://olimjonorifjonov.com/", "breakfast",],
+    //     details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    //     list: ["home", "hello", "bye", "asdsa", "dasds"],
 
-    },
-    {
-        id: 9,
-        links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
-        title: ["https://olimjonorifjonov.com/", "breakfast",],
-        details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        list: ["home", "hello", "bye", "asdsa", "dasds"],
+    // },
+    // {
+    //     id: 9,
+    //     links: ["https://olimjonorifjonov.com/", "https://olimjonorifjonov.com/"],
+    //     title: ["https://olimjonorifjonov.com/", "breakfast",],
+    //     details: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    //     list: ["home", "hello", "bye", "asdsa", "dasds"],
 
-    },
+    // },
 
 ]
 // console.log(fetch("data.txt"))
@@ -125,31 +124,31 @@ function displayArchive(data, number) {
         listItemArray = listItemArray.join("")
         // console.log(listItemArray)
         let projectContent = `<div class="card">
-        <div class="icon-links">
-            <i class="fa-regular fa-folder"></i>
-            <ul class="icon-list">
-                <li>
-                    <a href="${item.links[0]}" target="_blank">
-                        <i class="fa-brands fa-github"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="${item.links[1]}" target="_blank">
-                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <a href="${item.title[0]}">
-            <h3 class="headline">
-                ${item.title[1]}
-            </h3>
-        </a>
-        <p class="description">${item.details}</p>
-        <ul class="prog-names">
-            ${listItemArray}
-        </ul>
-    </div>`
+                                    <div class="icon-links">
+                                        <i class="fa-regular fa-folder"></i>
+                                        <ul class="icon-list">
+                                            <li>
+                                                <a href="${item.links[0]}" target="_blank">
+                                                    <i class="fa-brands fa-github"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="${item.links[1]}" target="_blank">
+                                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <a href="${item.title[0]}">
+                                        <h3 class="headline">
+                                            ${item.title[1]}
+                                        </h3>
+                                    </a>
+                                    <p class="description">${item.details}</p>
+                                    <ul class="prog-names">
+                                        ${listItemArray}
+                                    </ul>
+                                </div>`
 
         if (item.id <= number) {
             return projectContent;
